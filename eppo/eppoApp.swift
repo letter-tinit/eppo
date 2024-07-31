9123a029fcb40a1ff37956f68512d12c00145a8d
TiniT <letter.tinit@gmail.com>
2024-07-31 12:29:29 +0700

    first commit

---
+++ eppo/eppoApp.swift
@@ -0,0 +1,17 @@
+//
+//  eppoApp.swift
+//  eppo
+//
+//  Created by Letter on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct eppoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
