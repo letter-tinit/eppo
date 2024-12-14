9ecdd462bb95ade480759d02b66b2ab4cc3604c4
TiniT <letter.tinit@gmail.com>
2024-12-14 15:27:40 +0700

    FIXED: Loading Feedback Submit

--- Eppo/View/Order/RatingSubmitView.swift
+++ Eppo/View/Order/RatingSubmitView.swift
@@ -17,108 +17,115 @@ struct RatingSubmitView: View {
     init(viewModel: RatingSubmitViewModel) {
         self.viewModel = viewModel
     }
-  
+    
     // MARK: - BODY
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            CustomHeaderView(title: "Viết đánh giá")
-            
-            Text("Đánh giá bài viết")
-                .font(.headline)
-                .padding(10)
-                .foregroundStyle(.blue)
-            
-            TextEditor(text: $viewModel.textRating)
-                .onChange(of: viewModel.textRating) { newValue, oldValue in
-                    if newValue.count > viewModel.textLimit {
-                        viewModel.textRating = String(newValue.prefix(viewModel.textLimit))
-                    }
-                    print(viewModel.textRating)
-                }
-                .scrollContentBackground(.hidden)
-                .padding(10)
-                .frame(maxWidth: .infinity)
-                .frame(height: 260)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.mint, lineWidth: 3)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color(uiColor: UIColor.systemGray6)))
-                )
-                .foregroundStyle(.black)
-                .tint(.black)
-                .padding(10)
-                .overlay(alignment: .bottomTrailing) {
-                    Text("\(viewModel.textRating.count)/\(viewModel.textLimit)")
-                        .padding([.bottom, .trailing])
-                        .foregroundStyle(.darkBlue)
-                }
-                .toolbar {
-                    ToolbarItemGroup(placement: .keyboard) {
-                        Spacer()
-                        Button("Hoàn tất") {
-                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                        }
-                        .fontWeight(.medium)
-                    }
-                }
-            
-            RatingStarSubmitionView(rating: $viewModel.rating)
-                .padding(10)
-            
-            // Additional Images Picker
-            VStack(alignment: .leading) {
-                Text("Hình ảnh bổ sung").font(.headline)
-                PhotosPicker(selection: $additionalImagePickerItems, matching: .images) {
-                    Image(systemName: "photo.on.rectangle.angled")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                        .foregroundColor(.blue)
-                }
-                .onChange(of: additionalImagePickerItems) { _, newValue in
-                    Task {
-                        await viewModel.handleAdditionalImagesPicker(newValue)
-                    }
-                }
+        ZStack {
+            VStack(alignment: .leading, spacing: 0) {
+                CustomHeaderView(title: "Viết đánh giá")
                 
-                // Display Selected Additional Images
-                ScrollView(.horizontal) {
-                    HStack {
-                        ForEach(viewModel.additionalImages, id: \.self) { image in
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 100, height: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                Text("Đánh giá bài viết")
+                    .font(.headline)
+                    .padding(10)
+                    .foregroundStyle(.blue)
+                
+                TextEditor(text: $viewModel.textRating)
+                    .onChange(of: viewModel.textRating) { newValue, oldValue in
+                        if newValue.count > viewModel.textLimit {
+                            viewModel.textRating = String(newValue.prefix(viewModel.textLimit))
+                        }
+                        print(viewModel.textRating)
+                    }
+                    .scrollContentBackground(.hidden)
+                    .padding(10)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 260)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.mint, lineWidth: 3)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color(uiColor: UIColor.systemGray6)))
+                    )
+                    .foregroundStyle(.black)
+                    .tint(.black)
+                    .padding(10)
+                    .overlay(alignment: .bottomTrailing) {
+                        Text("\(viewModel.textRating.count)/\(viewModel.textLimit)")
+                            .padding([.bottom, .trailing])
+                            .foregroundStyle(.darkBlue)
+                    }
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button("Hoàn tất") {
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }
+                            .fontWeight(.medium)
                         }
                     }
-                }
-            }
-            .padding(.horizontal)
-            
-            Button {
-                viewModel.submitRating {
-                    dismiss()
-                }
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 8)
-                        .frame(height: 60)
-                        .foregroundStyle(
-                            LinearGradient(colors: [.lightBlue, .darkBlue], startPoint: .leading, endPoint: .trailing)
-                        )
+                
+                RatingStarSubmitionView(rating: $viewModel.rating)
+                    .padding(10)
+                
+                // Additional Images Picker
+                VStack(alignment: .leading) {
+                    Text("Hình ảnh bổ sung").font(.headline)
+                    PhotosPicker(selection: $additionalImagePickerItems, matching: .images) {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.blue)
+                    }
+                    .onChange(of: additionalImagePickerItems) { _, newValue in
+                        Task {
+                            await viewModel.handleAdditionalImagesPicker(newValue)
+                        }
+                    }
                     
-                    Text("Gửi đánh giá")
-                        .foregroundStyle(.white)
-                        .font(.system(size: 20, weight: .semibold))
-                        .fontDesign(.rounded)
+                    // Display Selected Additional Images
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(viewModel.additionalImages, id: \.self) { image in
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                            }
+                        }
+                    }
                 }
-                .padding()
-                .padding(.top, 30)
+                .padding(.horizontal)
                 
-            } // SUBMIT RATING BUTTON
+                Button {
+                    viewModel.submitRating {
+                        dismiss()
+                    }
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 8)
+                            .frame(height: 60)
+                            .foregroundStyle(
+                                LinearGradient(colors: [.lightBlue, .darkBlue], startPoint: .leading, endPoint: .trailing)
+                            )
+                        
+                        Text("Gửi đánh giá")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 20, weight: .semibold))
+                            .fontDesign(.rounded)
+                    }
+                    .padding()
+                    .padding(.top, 30)
+                    
+                } // SUBMIT RATING BUTTON
+                
+                Spacer()
+            }
+            .blur(radius: viewModel.isLoading ? 2 : 0)
+            .disabled(viewModel.isLoading ? 1 : 0)
             
-            Spacer()
+            LoadingCenterView()
+                .opacity(viewModel.isLoading ? 1 : 0)
         }
         .ignoresSafeArea(.container, edges: .vertical)
         .navigationBarBackButtonHidden()
